8459df09d3fbfe20a078f6546709e6a786d1ad7c
gustavoprivattofranco <gustavoprivattofranco@gmail.com>
2025-10-22 13:54:19 -0300

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-
 function toggleMenu() {
-    const navUl = document.querySelector('nav ul');
-    navUl.classList.toggle('show');
+    const navList = document.querySelector('.nav-list');
+    navList.classList.toggle('show');
 }
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const currentPage = window.location.pathname.split('/').pop() || 'index.html';
-    const links = document.querySelectorAll('nav a');
-    links.forEach(link => {
-        if (link.getAttribute('href') === currentPage) {
-            link.classList.add('active');
-        }
-    });
-});
